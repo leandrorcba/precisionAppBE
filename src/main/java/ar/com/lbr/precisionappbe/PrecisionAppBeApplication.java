6469b6df91ca6458a1a48aa8a3199dbd31aca274
Leandro Rebolini <lrebolini@gmail.com>
2026-02-25 08:19:51 -0300

    first commit

---
+++ src/main/java/ar/com/lbr/precisionappbe/PrecisionAppBeApplication.java
@@ -0,0 +1,13 @@
+package ar.com.lbr.precisionappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecisionAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrecisionAppBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/com/lbr/precisionappbe/PrecisionAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.lbr.precisionappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecisionAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
