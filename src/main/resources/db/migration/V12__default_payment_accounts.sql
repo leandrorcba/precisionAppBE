b14c3c490e3263ebcc838035439059b1f2702ebb
Leandro Rebolini <lrebolini@gmail.com>
2026-06-04 19:53:57 -0300

    time management

--- src/main/resources/db/migration/V12__default_payment_accounts.sql
+++ src/main/resources/db/migration/V12__default_payment_accounts.sql
@@ -1,13 +1,15 @@
-INSERT INTO `cuenta_bancaria` (`banco`, `alias_cbu`, `cbu`, `numero_cuenta`, `moneda`, `habilitada`) VALUES
-('Banco Galicia', 'galicia.alias', '0070000000000000000001', '12345/6', 'ARS', 1),
-('Banco Santander', 'santander.alias', '0720000000000000000001', '65432/1', 'ARS', 1);
+INSERT INTO `cuenta_bancaria` (`banco`, `alias_cbu`, `cbu`, `numero_cuenta`, `moneda`, `habilitada`)
+VALUES ('Cuenta Banco Negocio', 'Negocio.alias', '0070000000000000000001', '12345/1', 'ARS', 1),
+       ('Cuenta Banco Bruno', 'Bruno.alias', '0070000000000000000002', '12345/2', 'ARS', 1),
+       ('Cuenta Banco Sofi', 'Sofi.alias', '0070000000000000000003', '12345/3', 'ARS', 1),
+       ('Cuenta Banco Ro', 'Ro.alias', '0070000000000000000004', '12345/4', 'ARS', 1),
+       ('Cuenta Banco Lucho', 'Lucho.alias', '0720000000000000000005', '65432/5', 'ARS', 1);
 
-INSERT INTO `tarjeta` (`nombre`) VALUES
-('Visa Crédito'),
-('Visa Débito'),
-('Mastercard Crédito'),
-('Mastercard Débito');
+INSERT INTO `tarjeta` (`nombre`)
+VALUES ('Visa Crédito'),
+       ('Visa Débito'),
+       ('Mastercard Crédito'),
+       ('Mastercard Débito');
 
-INSERT INTO `mercado_pago` (`titular`, `disabled`) VALUES
-('Precision Cut MP', 0),
-('Admin Personal MP', 0);
+INSERT INTO `mercado_pago` (`titular`, `disabled`)
+VALUES ('Precision MP', 0)
